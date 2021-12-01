ae995143446d6face079871bb970e0fc12fcdb37
martindotslash <martin@dotslash.co.za>
2021-12-01 10:39:29 +0200

    Updating Profile

---
+++ CodeChallenges/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+namespace String_Builder
+{
+    public class Solution
+    {
+        static void Main(string[] strs)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string[] array = new string[3] { "flower", "flow", "flight" };
+            int[] nums = new int[] { 3, 2, 2, 3 };
+            var val = 3;
+            int k = RemoveElement(nums, val);
+        }
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int i = 0;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] != val)
+                {
+                    nums[i] = nums[j];
+                    i++;
+                }
+                
+            }
+
+            return i;
+        }
+
+    }
+}
